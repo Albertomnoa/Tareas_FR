ca029b37f15e409d3de6fbabc521a059f885fce0
Albertomnoa <47126819+Albertomnoa@users.noreply.github.com>
2019-05-25 23:58:01 -0500

    areglo1

--- Tarea_1/grafo9.py
+++ Tarea_1/grafo9.py
@@ -10,26 +10,26 @@ A.add_edge('Maria(15)','Yanet(17)'  ,weight=2)
 A.add_edge('Yanet(17)','Yanet(17)'  ,weight=2)
 
 
-A.add_edge('Maria(15)','Ferndo(16)' ,weight=2)
-A.add_edge('Ferndo(16)','Desisy(18)',weight=3)
-A.add_edge('Ferndo(16)','Desisy(18)',weight=2)
+A.add_edge('Maria(15)','Fernado(16)' ,weight=2)
+A.add_edge('Fernado(16)','Desisy(18)',weight=3)
+A.add_edge('Fernado(16)','Desisy(18)',weight=2)
 A.add_edge('Pedro(17)','Pedro(17)'  ,weight=2)
 A.add_edge('Pedro(17)','Julia(18)'  ,weight=2)
 A.add_edge('Pedro(17)','Rosi(16)'   ,weight=3)
 A.add_edge('Pedro(17)','Rosi(16)'   ,weight=2)
-A.add_edge('Ferndo(16)','Claudia(16)',weight=2)
+A.add_edge('Fernado(16)','Claudia(16)',weight=2)
 
-nodes_reflex = {'Luis(15)','Ferndo(16)','Julia(18)'}
+nodes_reflex = {'Luis(15)','Fernado(16)','Julia(18)'}
 nodes_no_reflex = {'Maria(15)','Yanet(17)','Rosi(16)','Desisy(18)',
                    'Pedro(17)','Claudia(16)'}
 
 black=[('Maria(15)','Yanet(17)'),
-  ('Maria(15)','Ferndo(16)'),('Ferndo(16)','Desisy(18)'),
+  ('Maria(15)','Fernado(16)'),('Fernado(16)','Desisy(18)'),
   ('Yanet(17)','Julia(18)'),('Desisy(18)','Pedro(17)'),
   ('Pedro(17)','Julia(18)'),  ('Pedro(17)','Rosi(16)'),
-  ('Ferndo(16)','Claudia(16)')]
+  ('Fernado(16)','Claudia(16)')]
 
-bl=[('Ferndo(16)','Desisy(18)'),('Pedro(17)','Rosi(16)')]
+bl=[('Fernado(16)','Desisy(18)'),('Pedro(17)','Rosi(16)')]
 
 posicion=nx.spring_layout(A)
 nx.draw_networkx_nodes(A,posicion,nodelist=nodes_reflex,
